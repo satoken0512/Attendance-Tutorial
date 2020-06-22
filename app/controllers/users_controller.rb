0323cc2906fa033cdb70a15eda625142a0219b3d
齋藤健太 <satoken.0512@gmail.com>
2020-06-22 15:13:41 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
- before_action :set_user, only: [:show, :edit, :update,:destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update,:destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   
   def index
     @users = User.paginate(page: params[:page])
   end
   
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -20,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = '新規作成に成功しました'
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
@@ -28,10 +27,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -45,11 +43,29 @@ class UsersController < ApplicationController
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
+  
+  def edit_basic_info
+  end
+
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
+  end
+
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
   end
+  
+  def basic_info_params
+    params.require(:user).permit(:department, :basic_time, :work_time)
+  end
+  
   
   # beforeフィルター
   
